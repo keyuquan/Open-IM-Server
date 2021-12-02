3e3ce68c638a91ee31edaceb977a0ee5d2ead4e6
keyuquan <929842382@qq.com>
2021-12-02 20:22:47 +0800

    清空代码

--- run.sh
+++
@@ -1,3 +0,0 @@
-tar zxvf Open-IM-Server.tgz -C ./
-rm   -rf  Open-IM-Server.tgz
-docker-compose down && docker-compose up -d --build
--- script/docker_check_service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "docker-compose ps..........................."
-docker-compose ps
-
-sleep 20
-
-echo "check OpenIM................................."
-./check_all.sh
-
